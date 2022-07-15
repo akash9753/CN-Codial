66e072aa63f531bc29114fcfc9fcf590b2dab92d
akash patel <akash.patel@hurix.com>
2022-07-15 23:54:38 +0530

    settings

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,40 +1,89 @@
+import { useState } from 'react';
 import styles from '../styles/settings.module.css';
-import { useAuth } from "../providers/AuthProvider";
-const Settings = () =>{
-    const auth = useAuth()
-    console.log(auth.user);
-    return (
-        <div className={styles.settings}>
-     <div className={styles.imgContainer}>
-          <img src="https://cdn-icons-png.flaticon.com/512/2922/2922510.png" alt="" />
-     </div>
+import { useAuth } from '../providers/AuthProvider';
+const Settings = () => {
+    const auth = useAuth();
+  const [editMode, setEditMode] = useState(false);
+  const [name, setName] = useState(auth.user?.name ? auth.user.name : '');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [savingForm, setSavingForm] = useState(false);
 
-     <div className={styles.field}>
-         <div className={styles.fieldName}>Email</div>
-         <div className={styles.fieldValue}>{auth.user?.email}</div>
-     </div>
+  const updateProfile = () => {};
+  console.log(auth.user);
+  return (
+    <div className={styles.settings}>
+      <div className={styles.imgContainer}>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2922/2922510.png"
+          alt=""
+        />
+      </div>
 
-     <div className={styles.field}>
-         <div className={styles.fieldName}>Name</div>
-         <div className={styles.fieldValue}>{auth.user?.name}</div>
-     </div>
+      <div className={styles.field}>
+        <div className={styles.fieldLabel}>Email</div>
+        <div className={styles.fieldValue}>{auth.user?.email}</div>
+      </div>
 
-     <div className={styles.field}>
-         <div className={styles.fieldName}>Password</div>
-         <input type="password" />
-     </div>
+      <div className={styles.field}>
+        <div className={styles.fieldLabel}>Name</div>
+        {editMode ? (
+          <input
+            type="text"
+            value={name}
+            onChnage={(e) => setName(e.target.value)}
+          />
+        ) : (
+          <div className={styles.fieldValue}>{auth.user?.name}</div>
+        )}
+      </div>
 
-     <div className={styles.field}>
-         <div className={styles.fieldName}>Confirm Password</div>
-         <input type="passowrd" />
-     </div>
-     
-     <div className={styles.btnGrp}>
-         <button className={`button ${styles.editBtn}`}>Edit Profile </button>
-     </div>
+      {editMode && (
+        <>
+          <div className={styles.field}>
+            <div className={styles.fieldLabel}>Password</div>
+            <input
+            type="password"
+            value={password}
+            onChnage={(e) => setPassword(e.target.value)}
+          />
+          </div>
 
+          <div className={styles.field}>
+            <div className={styles.fieldLabel}>Confirm Password</div>
+            <input
+            type="password"
+            value={confirmPassword}
+            onChnage={(e) => setConfirmPassword(e.target.value)}
+          />
+          </div>
+        </>
+      )}
+
+      <div className={styles.btnGrp}>
+        {editMode ? (
+          <>
+            <button className={`button ${styles.saveBtn}`}>
+              {savingForm ? 'Saving profile...' : 'Save profile'}
+            </button>
+            <button
+              className={`button ${styles.editBtn}`}
+              onClick={() => setEditMode(false)}
+            >
+              Go Back
+            </button>
+          </>
+        ) : (
+          <button
+            className={`button ${styles.editBtn}`}
+            onClick={() => setEditMode(true)}
+          >
+            Edit Profile
+          </button>
+        )}
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Settings;
